59402d269a908903af722b40f362165562ac8738
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2022-03-24 20:30:13 +0530

    basic api ready

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,15 +1,8 @@
-// @desc    Get todos
-// @route   GET /api/todo
-// @access  Private
 const getTodos = (req, res) => {
     res.status(200).json({ message: "Get todo" })
 }
 
 const setTodo = (req, res) => {
-    if (!req.body.text) {
-        res.status(400).json({ message: 'Please add text' })
-        return
-    }
     res.status(200).json({ message: "Set todo" })
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-const express = require("express") // importing
-const dotenv = require("dotenv").config() // importing and stating use
-const app = express() // making an expression application 
-const PORT = process.env.PORT || 5000
+require("dotenv").config() // importing and stating use
 
+const express = require("express")
+const app = express() 
+
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/todo', require('./routes/todoRoutes'))
 
-// my app listening for requests on port PORT
 app.listen(PORT, () => {
     console.log(`Server stated on port: ${PORT}`)
 })
